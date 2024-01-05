f33fba509168f7ec7e9a400023750067454493fc
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-01-05 17:27:53 +0100

    Refactoring. Add a status atribute to Character() for future checking. Add a control for go() when no direction is given.

--- text-based-game.py
+++ text-based-game.py
@@ -7,6 +7,7 @@ orcs = False
 
 class Character:
     inventory = []
+    status = "normal"
     def __init__(self, name):
         self.name = name
 
@@ -22,7 +23,7 @@ class Character:
         if (item == 'torch') and (light == False):
             light = True
             global location
-            location = Hall()
+            location.description = location.lightdescription
             return print("You have lit the torch, now you can see what's around you... sort of.")
         elif (item == 'torch') and light:
             return print('The torch is already lit.')
@@ -51,24 +52,8 @@ class Room:
     def look(self):
         return print(self.description)
     def search(self):
-        if not self.item:
+        if not self.item or self.item == None:
             return "There's nothing else of interest around here"
-        
-    
-class GreatHall(Room):
-    def __init__(self):
-        super().__init__(
-            name="Great Hall",
-            description="Ahead of you stretches a great hall filled with columns so high that the light from your torch cannot reach their ends. The floor is littered with broken weapons and debris, the remnants of a great battle...",
-            item=True
-        )
-    def go(self, direction):
-        global location
-        if direction == "back":
-            location = hall
-            location.enter()
-        else:
-            print("You cannot go that way")
 
 class Hall(Room):
     def __init__(self):
@@ -77,8 +62,7 @@ class Hall(Room):
             description= "A small room gives you the quietest and gloomiest welcome, a deathly silence. The door is closed behind you, there is no turning back, you are alone in total darkness...",
             item= "torch" 
         )
-        if light :
-            self.description = "In the dim light of the torch, you can see that the hall divides into several paths: a narrow passage to the left, a wide corridor to the right, and stairs leading down to the forward end."
+        self.lightdescription = "In the dim light of the torch, you can see that the hall divides into several paths: a narrow passage to the left, a wide corridor to the right, and stairs leading down to the forward end."
     
     
 
@@ -86,6 +70,8 @@ class Hall(Room):
         global light
         if not light:
             print("You cannot see where to go in this darkness")
+        elif not direction:
+            print("Go where?")
         else:
             global location
             if direction == "right":
@@ -104,7 +90,27 @@ class Hall(Room):
         else:
             character.inventory.append(self.item)
             print(f"You stepped on something... You found a {self.item} lying in the ground, next to your feet.\n*'{self.item}' was added to your invetory*")
-            self.item = None
+            self.item = None        
+    
+class GreatHall(Room):
+    def __init__(self):
+        super().__init__(
+            name="Great Hall",
+            description="Ahead of you stretches a great hall filled with columns so high that the light from your torch cannot reach their ends. The floor is littered with broken weapons and debris, the remnants of a great battle...",
+            item=False
+        )
+    def go(self, direction):
+        global location
+        if direction == "back":
+            location = hall
+            location.enter()
+        else:
+            print("You cannot go that way")
+
+    # def search(self):
+    #     pass
+
+
 
 
 # Declaring the rooms and the character    
@@ -114,37 +120,37 @@ hall = Hall()
 
 # Introduction: title, tutorial and first riddle
 f = Figlet(font='slant')
-print(f.renderText('Lost in Moria'))
-print(
-    "Welcome to Lost in Moria, a text-based adventure game built while I was learning Python.\nAs a text-based adventure, you're expected to enter what your character will do in the form of text. \nThis game accepts some words like 'go', 'search', 'look' and others related to the items you might find during your adventure.\nThis symbol ('> ') indicates when you are expected to enter a command, if you don't see this symbol it probably means that the program is loading or the game is finished.\nHave fun and good luck!"
-    )
-print(
-    "You stand before the famous Doors of Durin... they appear to be locked, but an Elvish engraving presents you with a well-known riddle:"
-)
-print(
-    "Pedo mellon a minno... Say friend an enter..."
-)
+# print(f.renderText('Lost in Moria'))
+# print(
+#     "Welcome to Lost in Moria, a text-based adventure game built while I was learning Python.\nAs a text-based adventure, you're expected to enter what your character will do in the form of text. \nThis game accepts some words like 'go', 'search', 'look' and others related to the items you might find during your adventure.\nThis symbol ('> ') indicates when you are expected to enter a command, if you don't see this symbol it probably means that the program is loading or the game is finished.\nHave fun and good luck!"
+#     )
+# print(
+#     "You stand before the famous Doors of Durin... they appear to be locked, but an Elvish engraving presents you with a well-known riddle:"
+# )
+# print(
+#     "Pedo mellon a minno... Say friend an enter..."
+# )
 
-while True:
-    answer = input("> ").strip().lower()
-    if answer != "mellon":
-        print(
-            "Nothing happens... the wind howls..."
-        )
-        continue
-    else:
-        print(
-            "The heavy stone doors open slowly, revealing nothing but dense darkness. Do you enter?"
-        )
-        break
-while True:
-    answer = input("> ").strip().lower()
-    if answer == "yes":
-        break
-    else:
-        print(
-            "Fine... whenever you are ready..."
-        )
+# while True:
+#     answer = input("> ").strip().lower()
+#     if answer != "mellon":
+#         print(
+#             "Nothing happens... the wind howls..."
+#         )
+#         continue
+#     else:
+#         print(
+#             "The heavy stone doors open slowly, revealing nothing but dense darkness. Do you enter?"
+#         )
+#         break
+# while True:
+#     answer = input("> ").strip().lower()
+#     if answer == "yes":
+#         break
+#     else:
+#         print(
+#             "Fine... whenever you are ready..."
+#         )
 
 # The main game
 # The game starts at the hall
